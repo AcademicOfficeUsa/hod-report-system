be0bfbe9e8b24c2349ceb1ce547abdd2b64d618c
academicofficeSmith <stephenn255@gmail.com>
2026-06-28 15:21:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hod-report-system/',
   optimizeDeps: {
     exclude: ['lucide-react'],
     include: ['xlsx'],
